be17f296ca77b8d4dde61ee97083f3b05965a170
wsp <810100157@qq.com>
2021-02-26 17:24:05 +0800

    doc: add usage

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # toolkit
+
+Commonly used functions.
+
+## Usage
+
+```npm
+npm i wsp-toolkit
+```
+
+```javascript
+import { isExist } from "wsp-toolkit";
+```
--- src/mustArray.ts
+++ src/mustArray.ts
@@ -1,4 +1,11 @@
+import {isExist} from "./index";
+
 export function mustArray<T>(x: any): Array<T> {
-    if (!Array.isArray(x)) return [];
-    return x;
+    if (Array.isArray(x)) return x;
+    if (!isExist(x)) return [];
+    try {
+      return Array.from(x);
+    } catch (e) {
+      return [];
+    }
 }
