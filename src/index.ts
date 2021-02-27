24b282651bad31b8d84f5e0e52bc4ddee453e6c8
wsp <810100157@qq.com>
2021-02-27 17:49:41 +0800

    refactor: 修改导出方式

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,9 @@
-import {isExist} from "./isExist";
-import {isEmpty} from "./isEmpty";
-import {mustArray} from "./mustArray";
-import {typeOf} from "./typeOf";
-import {identity} from "./identity";
-import {toNumber} from "./toNumber";
-import {add} from "./add";
-import {isNumber} from "./isNumber";
-import {isSafeNumber} from "./isSafeNumber";
-
-export {
-  add,
-  isNumber,
-  isSafeNumber,
-  identity,
-  isEmpty,
-  isExist,
-  mustArray,
-  typeOf,
-  toNumber,
-}
+export {isExist} from "./isExist";
+export {isEmpty} from "./isEmpty";
+export {mustArray} from "./mustArray";
+export {typeOf} from "./typeOf";
+export {identity} from "./identity";
+export {toNumber} from "./toNumber";
+export {add} from "./add";
+export {isNumber} from "./isNumber";
+export {isSafeNumber} from "./isSafeNumber";
--- src/isSafeNumber.ts
+++ src/isSafeNumber.ts
@@ -1,4 +1,4 @@
-import {isNumber} from "./isNumber";
+import {isNumber} from "./index";
 
 export function isSafeNumber(x: any): boolean {
   return isNumber(x) && Number.MIN_SAFE_INTEGER <= x && x <= Number.MAX_SAFE_INTEGER;
