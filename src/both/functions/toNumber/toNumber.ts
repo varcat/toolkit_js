93eb89f92711cca5caad0664b4766d5db176cda0
shunping <810100157@qq.com>
2021-12-21 18:07:08 +0800

    feat: toNumber 增加精度处理

--- src/both/functions/toNumber/toNumber.ts
+++ src/both/functions/toNumber/toNumber.ts
@@ -1,17 +1,21 @@
 import { isSafeNumber } from "../isSafeNumber/isSafeNumber";
 import { isNumber } from "../isNumber/isNumber";
+import { isExist } from "../isExist/isExist";
 
 export interface IToNumberParams {
   defaultValue: number;
+  precision?: number;
 }
 
-export function toNumber(
-  x: any,
-  { defaultValue = 0 } = {} as IToNumberParams
-): number {
+export function toNumber(x: any, options = {} as IToNumberParams): number {
   if (typeof x === "string") {
     x = x.replace(/[^-0-9.]/g, "");
   }
+  x.lastIndexOf(".");
+  if (isExist(options.precision)) {
+    const n = x[options.precision! + 1];
+  }
   const num = isNumber(x) ? x : Number(x);
-  return isSafeNumber(num) ? num : defaultValue;
+  if (!isSafeNumber(num)) return options.defaultValue;
+  return num;
 }
