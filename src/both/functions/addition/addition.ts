f40519e5c2479fedc7eed8f7afcad7062aa557f9
shunping <810100157@qq.com>
2022-03-01 16:38:49 +0800

    feat: addition 函数增加对合法数字字符串的处理

--- src/both/functions/addition/addition.ts
+++ src/both/functions/addition/addition.ts
@@ -1,10 +1,16 @@
-export function addition(...numbers: number[]): number {
+import { toNumber } from "../toNumber/toNumber";
+
+export function addition(numbers: number[]): number;
+export function addition(...numbers: number[]): number;
+export function addition(numbers: any): number {
   let result = 0;
 
   function add(xs: number[]) {
     for (let i = 0; i < xs.length; i += 1) {
       if (typeof xs[i] === "number") {
         result += xs[i];
+      } else if (typeof xs[i] === "string") {
+        result += toNumber(xs[i]);
       } else if (Array.isArray(xs[i])) {
         add(xs[i] as unknown as number[]);
       }
