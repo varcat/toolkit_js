9e1dce9e330b94695278a0b14748636838ef8af5
shunping <810100157@qq.com>
2022-04-10 10:42:10 +0800

    feat(currying): currying函数可以正确显示函数签名

--- src/both/functions/currying/currying.test.ts
+++ src/both/functions/currying/currying.test.ts
@@ -1,12 +1,16 @@
-import {currying} from "./currying";
+import { currying } from "./currying";
 
 function add(l: number, r: number): number {
   return l + r;
 }
 
-test('', () => {
+test("", () => {
   const fn = currying(add);
   expect(fn(1, 2)).toBe(3);
   expect(fn(1)(2)).toBe(3);
   expect(fn()(1)(2)).toBe(3);
+  const b = currying((a: string, b: number, d: string) => a.repeat(b) + d);
+  const c = b("A");
+  const d = c(2);
+  expect(d("S")).toBe("AAS");
 });
--- src/both/functions/currying/currying.ts
+++ src/both/functions/currying/currying.ts
@@ -1,9 +1,62 @@
-export function currying<T extends Function>(fn: T): T {
-  const arity = fn.length;
-  return function curry(...args: any[]): any {
-    if (args.length < arity) {
-      return curry.bind(null, ...args);
+type PartialTuple<TUPLE extends any[], EXTRACTED extends any[] = []> =
+  // If the tuple provided has at least one required value
+  TUPLE extends [infer NEXT_PARAM, ...infer REMAINING]
+    ? // recurse back in to this type with one less item
+      // in the original tuple, and the latest extracted value
+      // added to the extracted list as optional
+      PartialTuple<REMAINING, [...EXTRACTED, NEXT_PARAM?]>
+    : // else if there are no more values,
+      // return an empty tuple so that too is a valid option
+      [...EXTRACTED, ...TUPLE];
+
+type PartialParameters<FN extends (...args: any[]) => any> = PartialTuple<
+  Parameters<FN>
+>;
+
+type RemainingParameters<PROVIDED extends any[], EXPECTED extends any[]> =
+  // if the expected array has any required items…
+  EXPECTED extends [infer E1, ...infer EX]
+    ? // if the provided array has at least one required item
+      PROVIDED extends [infer P1, ...infer PX]
+      ? // if the type is correct, recurse with one item less
+        //in each array type
+        P1 extends E1
+        ? RemainingParameters<PX, EX>
+        : // else return this as invalid
+          never
+      : // else the remaining args is unchanged
+        EXPECTED
+    : // else there are no more arguments
+      [];
+
+type CurriedFunctionOrReturnValue<
+  PROVIDED extends any[],
+  FN extends (...args: any[]) => any
+> = RemainingParameters<PROVIDED, Parameters<FN>> extends [any, ...any[]]
+  ? CurriedFunction<PROVIDED, FN>
+  : ReturnType<FN>;
+
+type CurriedFunction<
+  PROVIDED extends any[],
+  FN extends (...args: any[]) => any
+> = <
+  NEW_ARGS extends PartialTuple<RemainingParameters<PROVIDED, Parameters<FN>>>
+>(
+  ...args: NEW_ARGS
+) => CurriedFunctionOrReturnValue<[...PROVIDED, ...NEW_ARGS], FN>;
+
+export function currying<
+  FN extends (...args: any[]) => any,
+  STARTING_ARGS extends PartialParameters<FN>
+>(
+  targetFn: FN,
+  ...existingArgs: STARTING_ARGS
+): CurriedFunction<STARTING_ARGS, FN> {
+  return function (...args) {
+    const totalArgs = [...existingArgs, ...args];
+    if (totalArgs.length >= targetFn.length) {
+      return targetFn(...totalArgs);
     }
-    return fn(...args);
-  } as unknown as T;
+    return currying(targetFn, ...(totalArgs as PartialParameters<FN>));
+  };
 }
