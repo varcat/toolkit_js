f538c8c33cca3500ffc848cb1927a7610d44f0f6
shunping <810100157@qq.com>
2022-04-19 16:29:10 +0800

    feat: safaLast增加空数组判断

--- src/functions/safeLast/index.ts
+++ src/functions/safeLast/index.ts
@@ -1,4 +1,4 @@
 export function safeLast<T>(xs: T[]): T | undefined {
-  if (!Array.isArray(xs)) return undefined;
-  return xs?.[xs?.length - 1];
+  if (!Array.isArray(xs) || xs.length === 0) return undefined;
+  return xs[xs.length - 1];
 }
