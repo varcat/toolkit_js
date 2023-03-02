6245b19e1fdf8e858afd237a85276df77b8ea17a
wangshunping <810100157@qq.com>
2023-03-02 14:03:47 +0800

    feat: rename movePrecision

--- src/functions/multipliedBy/index.ts
+++ src/functions/multipliedBy/index.ts
@@ -1,4 +1,5 @@
-import { getPrecision, processPrecision } from "../plus";
+import { getPrecision } from "../plus";
+import {movePrecision} from "../../internal/movePrecision";
 
 export function multipliedBy(a: string, b: string): string {
   const ap = getPrecision(a);
@@ -6,5 +7,5 @@ export function multipliedBy(a: string, b: string): string {
   const precision = ap + bp;
   const aVal = a.replace(".", "");
   const bVal = b.replace(".", "");
-  return processPrecision((BigInt(aVal) * BigInt(bVal)).toString(), precision);
+  return movePrecision((BigInt(aVal) * BigInt(bVal)).toString(), precision);
 }
--- src/functions/plus/index.ts
+++ src/functions/plus/index.ts
@@ -1,24 +1,9 @@
+import {movePrecision} from "../../internal/movePrecision";
+
 export const getPrecision = (x: string): number => {
   const idx = x.indexOf(".");
   if (idx === -1) return 0;
   return x.length - idx - 1;
-};
-
-export const processPrecision = (x: string, p: number): string => {
-  if (p === 0) return x;
-  const hasSymbol = x[0] === "-";
-  const val = hasSymbol ? x.slice(1) : x;
-  let num: string;
-  if (p > val.length) {
-    num = `0.${"0".repeat(p - val.length)}${val}`;
-  } else {
-    num =
-      (val.slice(0, val.length - p) || "0") + "." + val.slice(val.length - p);
-  }
-  return (
-    (hasSymbol ? "-" : "") +
-    num.replace(/([1-9])(0*)$/, "$1").replace(/\.0*$/, "")
-  );
 };
 
 export const plus = (a: string, b: string): string => {
@@ -27,5 +12,5 @@ export const plus = (a: string, b: string): string => {
   const precision = Math.max(ap, bp);
   const aVal = a.replace(".", "") + "0".repeat(precision - ap);
   const bVal = b.replace(".", "") + "0".repeat(precision - bp);
-  return processPrecision((BigInt(aVal) + BigInt(bVal)).toString(), precision);
+  return movePrecision((BigInt(aVal) + BigInt(bVal)).toString(), precision);
 };
